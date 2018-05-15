b06ca673047f95f57d7efd5d10ddbb3facea63cb
Rajesh Naroth <rajeshnaroth@gmail.com>
2018-05-15 14:28:56 -0700

    A bit of refactoring

--- meetup-3-functional-programming/lesson-1-compose/src/piglatin/piglatin.js
+++ meetup-3-functional-programming/lesson-1-compose/src/piglatin/piglatin.js
@@ -1,23 +1,26 @@
-const R = require('ramda')
+const R = require("ramda");
 
-const breakWord = (word) => {
-  word = word || ''
-  let head = word.split(/[aeiou]/gi)[0]
-  return ({
+const breakWord = word => {
+  word = word || "";
+  let head = R.head(word.split(/[aeiou]/gi)); // first item
+  return {
     head: head,
-    tail: word.substr(head.length),
-  })
-}
+    tail: word.substr(head.length)
+  };
+};
 
-const rearrange = ({ head, tail }) => `${tail}${head}`.toLowerCase()
+const rearrange = ({ head, tail }) => `${tail}${head}`;
 
-const attachAy = (word) => word ? `${word}ay` : word
+const toLowerCase = string => (string ? string.toLowerCase() : "");
 
-const pigLatin = R.pipe(breakWord, rearrange, attachAy)
+const attachAy = word => (word ? `${word}ay` : word);
+
+const pigLatin = R.pipe(breakWord, rearrange, toLowerCase, attachAy);
 
 module.exports = {
+  toLowerCase,
   pigLatin,
   breakWord,
   rearrange,
   attachAy
-}
+};
--- meetup-3-functional-programming/lesson-1-compose/src/piglatin/piglatin.test.js
+++ meetup-3-functional-programming/lesson-1-compose/src/piglatin/piglatin.test.js
@@ -1,33 +1,43 @@
-const { pigLatin, breakWord, rearrange, attachAy } = require('./piglatin')
+const {
+  pigLatin,
+  breakWord,
+  rearrange,
+  toLowerCase,
+  attachAy
+} = require("./piglatin");
 
 // Tests
-test('breakWord function', () => {
-  expect(breakWord('test').head).toBe('t')
-  expect(breakWord('test').tail).toBe('est')
-  expect(breakWord('ask').head).toBe('')
-  expect(breakWord('ask').tail).toBe('ask')
-})
+test("breakWord function", () => {
+  expect(breakWord("test").head).toBe("t");
+  expect(breakWord("test").tail).toBe("est");
+  expect(breakWord("ask").head).toBe("");
+  expect(breakWord("ask").tail).toBe("ask");
+});
 
-test('attachAy function', () => {
-  expect(attachAy('test')).toBe('testay')
-  expect(attachAy('')).toBe('')
-  expect(attachAy(null)).toBe(null)
-})
+test("attachAy function", () => {
+  expect(attachAy("test")).toBe("testay");
+  expect(attachAy("")).toBe("");
+  expect(attachAy(null)).toBe(null);
+});
 
-test('rearrange function', () => {
-  expect(rearrange({ head: 'a', tail: 'b' })).toBe('ba')
-})
+test("rearrange function", () => {
+  expect(rearrange({ head: "a", tail: "b" })).toBe("ba");
+});
 
-test('pigLatin function', () => {
+test("lowercase function", () => {
+  expect(toLowerCase("maryHadALittleLamb")).toBe("maryhadalittlelamb");
+});
+
+test("pigLatin function", () => {
   const testSuite = [
-    ['look', 'ooklay'],
-    ['bag', 'agbay'],
-    ['BAG', 'agbay'],
-    ['ant', 'antay'],
-    ['clarinet', 'arinetclay'],
-    ['spray', 'ayspray']
-  ]
-  testSuite.forEach((pair) => {
-    expect(pigLatin(pair[0])).toBe(pair[1])
-  })
-})
+    ["look", "ooklay"],
+    ["bag", "agbay"],
+    ["BAG", "agbay"],
+    ["ant", "antay"],
+    ["clarinet", "arinetclay"],
+    ["spray", "ayspray"]
+  ];
+  testSuite.forEach(pair => {
+    expect(pigLatin(pair[0])).toBe(pair[1]);
+  });
+});
